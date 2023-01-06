d343e278e107f8b24b01f3440482c4d9971b51ff
elina <aelin6277@gmail.com>
2023-01-07 00:33:19 +0100

    Initial Commit

---
+++ testing/testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  testingApp.swift
+//  testing
+//
+//  Created by Elin A on 2023-01-07.
+//
+
+import SwiftUI
+
+@main
+struct testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
